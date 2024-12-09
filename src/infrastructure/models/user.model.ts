a125814a36f9c7d3e8fdcc416e303c1bbce0a43f
Sadık HANECİOĞLU <sadik.hanecioglu@gmail.com>
2024-12-09 20:07:45 +0300

    mog model id fixed

--- src/infrastructure/models/user.model.ts
+++ src/infrastructure/models/user.model.ts
@@ -8,11 +8,20 @@ interface IUser extends Document {
 }
 
 const userSchema: Schema = new Schema<IUser>({
-    id: { type: String, required: true, default: uuidv4  },
+    //id: { type: String, required: true, default: uuidv4  },
     username: { type: String, required: true },
     email: { type: String, required: false },
     password: { type: String, required: true },
 })
+
+// `toJSON` ile `_id` alanını `id` olarak yeniden adlandırıyoruz ve `__v` gibi gereksiz alanları kaldırıyoruz.
+userSchema.set('toJSON', {
+    transform: (doc, ret) => {
+        ret.id = ret._id; // `_id` değerini `id` olarak atıyoruz
+        delete ret._id;   // `_id` alanını kaldırıyoruz
+        delete ret.__v;   // Mongoose tarafından eklenen `__v` alanını kaldırıyoruz
+    },
+});
 
 const UserModel = mongoose.model<IUser>('User', userSchema);
 
